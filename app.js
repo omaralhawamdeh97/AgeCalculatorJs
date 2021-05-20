610da38f10326048b44ccfe2b69b48463c929e5a
omaralhawamdeh97 <omar.alhawamdeh@gmail.com>
2021-05-20 03:08:59 +0300

    updated homework

--- app.js
+++ app.js
@@ -13,18 +13,36 @@
 
 // This functions should check the integrity of the parameters and pass true/false
 function checkParamsFn(year, month, day) {
-  // Write your code here
+  if (Number.isInteger(year))
+    if (Number.isInteger(month)) if (Number.isInteger(day)) return true;
+  return false;
 }
+console.log(checkParamsFn("199", 7, 5));
 
 // This functions checks if the person is or above 18 years of age, return true/false
 function checkOverEighteenFn(year, month, day) {
-  // Write your code here
+  let yearx = new Date();
+  let monthx = new Date();
+  let dayx = new Date();
+
+  if (yearx.getFullYear() < year + 18) return false;
+  else if (yearx.getFullYear() > year + 18) return true;
+  else if (monthx.getMonth() + 1 < month) return false;
+  else if (monthx.getMonth() + 1 > month) return true;
+  else if (dayx.getDate() < day) return false;
+  else if (dayx.getDate() >= day) return true;
 }
+console.log(checkOverEighteenFn(2003, 5, 20));
 
 function calculateAgeFn(year, month, day) {
-  // Write your code here
+  let yearx = new Date();
+  if (!checkParamsFn) return "error: check previous ";
+  else if (!checkOverEighteenFn) return "error: check previous";
+  else {
+    return yearx.getFullYear() - year;
+  }
 }
-
+console.log(calculateAgeFn(1997, 2, 24));
 // Look at the naming of the functions. it looks like salwaBaqer, where
 // the first letter of the first word is small, while the first letter of the
 // second word is Capital. This is called "Camel Casing."
